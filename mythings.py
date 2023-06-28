68626e5dc11f0511fc6b7e3a738c3009370b2ec2
denismp <denisputnam@gmail.com>
2023-06-28 08:15:04 -0400

    Began work on contact_info.

--- mythings.py
+++ mythings.py
@@ -1,29 +1,20 @@
 from tkinter import *
 from tkinter import ttk
+from utils.contact_info import ContactInfo
 
 from utils.thing_info import ThingInfo
 
-root = Tk(screenName="My Things", baseName="main window")
+root: Tk = Tk(screenName="My Things", baseName="main window")
 root.title("My Things To Remember")
 root.geometry("515x350")
 
-mainframe = ttk.Frame(root,padding="3 3 12 12")
+mainframe: Frame = ttk.Frame(root,padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 
-# thing_name = StringVar()
-# thing_entry = ttk.Entry(mainframe, width=15, textvariable=thing_name)
-# thing_entry.grid(column=2, row=1, sticky=(W, E))
-# ttk.Label(mainframe, text="Name").grid(column=0, row=1, sticky=W)
-
-# thing_type = StringVar()
-# thing_type_label = ttk.Label(mainframe, text="Type")
-# thing_type_combobox = ttk.Combobox(mainframe, values=["Bank", "Life Insurance"])
-# thing_type_label.grid(row=1, column=3)
-# thing_type_combobox.grid(row=1, column=4)
-name_obj = ThingInfo(mainframe,"Name", "Type")
-
+name_obj: ThingInfo = ThingInfo(mainframe,"Name", "Type")
+contact_obj: ContactInfo = ContactInfo(mainframe)
 
 
 root.mainloop()
--- utils/contact_info.py
+++ utils/contact_info.py
@@ -1,16 +1,19 @@
 from tkinter import E, W, Frame, StringVar, ttk
+import tkinter
 
 
 class ContactInfo:
 
     def __init__(self, frame: Frame):
         self.frame = frame
+        self.contact_info_frame = tkinter.LabelFrame(self.frame, text="Contact Information",padx=5)
         self.phone1 = StringVar()
-        self.phone1_entry = ttk.Entry(self.frame, width=15, textvariable=self.phone1)
-        self.phone1_entry.grid(column=2, row=1, sticky=(W, E))
-        ttk.Label(self.frame, text="Phone1").grid(column=0, row=1, sticky=W)
+        ttk.Label(self.contact_info_frame, text="Phone1").grid(column=0, row=0, sticky=W, padx=15)
+        self.phone1_entry = ttk.Entry(self.contact_info_frame, width=15, textvariable=self.phone1)
+        self.phone1_entry.grid(column=1, row=0, sticky=(W, E))
 
         self.phone2 = StringVar()
-        self.phone2_entry = ttk.Entry(self.frame, width=15, textvariable=self.phone2)
-        self.phone2_entry.grid(column=2, row=1, sticky=(W, E))
-        ttk.Label(self.frame, text="Phone1").grid(column=0, row=1, sticky=W)
+        ttk.Label(self.contact_info_frame, text="Phone2").grid(column=2, row=0, sticky=W, padx=15)
+        self.phone2_entry = ttk.Entry(self.contact_info_frame, width=15, textvariable=self.phone2)
+        self.phone2_entry.grid(column=3, row=0, sticky=(W, E))
+        self.contact_info_frame.pack(fill='x')
